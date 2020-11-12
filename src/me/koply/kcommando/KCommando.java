e0df44633055ecd63ba63be72f096ef8d50c02b9
musabrt <musafg@outlook.com>
2020-11-12 14:59:57 +0300

    release commit 2.3

--- src/me/koply/kcommando/KCommando.java
+++ src/me/koply/kcommando/KCommando.java
@@ -24,7 +24,7 @@ public final class KCommando {
 
     private final Params params = new Params();
     public static final Logger logger = Logger.getLogger("KCommando");
-    public static final String VERSION = "2.2";
+    public static final String VERSION = "2.3";
 
     public KCommando(@NotNull JDA jda) {
         params.setJda(jda);
